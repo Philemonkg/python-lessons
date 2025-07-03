01f5a76bc4762d33cb01bc48a316980c3e87468b
Philemonkg <manakaphilemon508@gmail.com>
2025-07-03 12:01:33 +0200

    add

---
+++ exception_handling.py
@@ -0,0 +1,12 @@
+
+try:
+    print(x)
+except NameError:
+    print("variable x is not defined")
+finally:
+    print("everithing went wrong")
+    
+x = -1
+
+if x < 0:
+    raise Exception("sorry, no numbers below zero")
